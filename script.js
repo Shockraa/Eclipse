05ae4283b3d63197c41eb498eeeb4cf7c31de1a7
Gustavo Oliveira <127700050+Shockraa@users.noreply.github.com>
2023-07-01 20:28:05 -0300

    Site visual Eclipse

---
+++ script.js
@@ -0,0 +1,10 @@
+alert('Este site foi feito para praticar as minhas habilidades visuais, os botões não possuem funcionalidade. A página de login está em outro repositório.');
+const loginButton = document.getElementById('loginButton');
+loginButton.addEventListener('click', function () {
+  alert('A página de login está em outro repositório');
+});
+
+const signupButton = document.getElementById('signupButton');
+signupButton.addEventListener('click', function () {
+  alert('A página de login está em outro repositório');
+});
